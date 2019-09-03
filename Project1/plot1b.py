19f1c73755288c7d7d47a30aec33d6e36dedad2e
Elin Finstad <elin.finstad@hotmail.com>
2019-09-03 21:29:54 +0200

    Generalizing the code and fixing the outlook

--- Project1/plot1b.py
+++ Project1/plot1b.py
@@ -1,34 +1,29 @@
-# This Python file uses the following encoding: utf-8
-
-# if__name__ == "__main__":
-#     pass
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-file = open("test.txt", "r")
-N = 10
 
-solution = np.array(N+1)
+def plot(filename, N):
+    file = open(filename, "r")
 
-h = 1.0/(N + 1)
-print(h)
-#i = 0
-#for line in file:
-#    line.rstrip('\n')
-#    print(line)
-#    solution[i] = int(line)
-#    i += 1
+    h = 1.0/(N + 1)
+    x = h*np.linspace(0, 1, N)
+    print(x)
 
-line = file.readline().split(",")
-for i in range(0, len(line)):
-    line[i] = float(line[i])
-print(line)
+    line = file.readline().split(",")
+
+    for i in range(0, len(line)):
+        line[i] = float(line[i])
 
 
-x = h*np.linspace(0, 1, N)
-plt.plot(x, line, label="N = "+str(N))
-plt.xlabel(x)
-plt.ylabel(solution)
-plt.legend()
-plt.show()
+    plt.plot(x, line, label="N = "+str(N))
+    plt.xlabel("x")
+    plt.ylabel("v(x)")
+    plt.legend()
+    plt.show()
+
+
+for i in (10, 100, 1000):
+    plot("b"+str(i)+".txt", i)
+
+
+
