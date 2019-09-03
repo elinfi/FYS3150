f6a393a67ccc1e0a89586c37badfec955a138826
Elin Finstad <elin.finstad@hotmail.com>
2019-09-03 13:20:48 +0200

    Adding code for plotting

--- Project1/plot1b.py
+++ Project1/plot1b.py
@@ -2,3 +2,33 @@
 
 # if__name__ == "__main__":
 #     pass
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+file = open("test.txt", "r")
+N = 10
+
+solution = np.array(N+1)
+
+h = 1.0/(N + 1)
+print(h)
+#i = 0
+#for line in file:
+#    line.rstrip('\n')
+#    print(line)
+#    solution[i] = int(line)
+#    i += 1
+
+line = file.readline().split(",")
+for i in range(0, len(line)):
+    line[i] = float(line[i])
+print(line)
+
+
+x = h*np.linspace(0, 1, N)
+plt.plot(x, line, label="N = "+str(N))
+plt.xlabel(x)
+plt.ylabel(solution)
+plt.legend()
+plt.show()
