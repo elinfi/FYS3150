dfd472178f3436656af8eb8d7e811d3023b8ca70
Elin Finstad <elin.finstad@hotmail.com>
2019-09-09 21:56:52 +0200

    Plot 1b and errors

--- Project1/plot1b.py
+++ Project1/plot1b.py
@@ -2,12 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot(filename, N):
+def plot(filename, N, name):
     file = open(filename, "r")
 
     h = 1.0/(N + 1)
-    x = h*np.linspace(0, 1, N)
-    print(x)
+    x = h*np.linspace(0, N+1, N+2)
 
     line = file.readline().split(",")
 
@@ -15,15 +14,27 @@ def plot(filename, N):
         line[i] = float(line[i])
 
 
-    plt.plot(x, line, label="N = "+str(N))
-    plt.xlabel("x")
-    plt.ylabel("v(x)")
-    plt.legend()
-    plt.show()
+    plt.plot(x, line, label=name)
 
 
 for i in (10, 100, 1000):
-    plot("b"+str(i)+".txt", i)
+    plot("1b-"+str(i)+".txt", i, "N = "+str(i))
+
+plot("analytic_solution-" + str(1000) + ".txt", i, "Analytic Solution")
+plt.title("General Thomas Algorithm")
+plt.xlabel("$x$")
+plt.ylabel("$v(x)$")
+plt.legend()
+plt.show()
 
 
+log_10_h = np.array([-1.04139, -2.00432, -3.00043, -4.00004, -5, -6])
+log_10_eps = np.array([-1.1797, -3.08804, -5.08005, -7.07936, -9.0049, -6.77137])
+
+plt.plot(log_10_h, log_10_eps)
+plt.plot(log_10_h, log_10_eps, "bo")
+plt.title("Maximum Relative Error")
+plt.xlabel("$log_{10}(h)$")
+plt.ylabel("max($\epsilon$)")
+plt.show()
 
