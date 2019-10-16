29da58a98c27761fc0715dbea15f660db0a17930
elinfi <elin.finstad@hotmail.com>
2019-10-16 16:59:18 +0200

    Adding code for running gauss_quadrature and improved_gauss_quadrature

--- Project3/main-program/main.cpp
+++ Project3/main-program/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include "gauss_legendre.h"
+#include "gauss_quadrature.h"
+#include "improved_gauss_quadrature.h"
+#include <cmath>
 
 using namespace std;
 
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int n = 10;
+//    double lambda = 2.0;
+
+//    double sum = gauss_quadrature(n, lambda);
+    double sum = improved_gauss_quadrature(n);
+
+    cout << "N = " << n << endl;
+//    cout << "lambda = " << lambda << endl;
+    cout << sum << endl;
+
+    double pi = acos(-1);
+    double exact = (5*pi*pi)/(16*16);
+
+    cout << abs(exact - sum) << endl;
+
     return 0;
 }
