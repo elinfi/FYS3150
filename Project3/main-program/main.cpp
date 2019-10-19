f347f2ff89bef916ddd686f19731272ed93a1be4
elinfi <elin.finstad@hotmail.com>
2019-10-19 16:13:32 +0200

    Put calls to the different methods in separeted functions

--- Project3/main-program/main.cpp
+++ Project3/main-program/main.cpp
@@ -6,85 +6,225 @@
 #include "improved_monte_carlo.h"
 #include <cmath>
 #include <random>
-//#include <mpi.h>
+#include <mpi.h>
+#include <chrono>
 
 using namespace std;
 
-
-int main()
-{
-    /*
-    // Gauss Quadrature
-    int n = 25;
-    double lambda = 2.7;
-    bool timing = true;
-
-    double sum = gauss_quadrature(n, lambda, timing);
+void print_gauss_quatrature(int n, double lambda, bool timing) {
+    cout << "Gauss Quadrature" << endl;
 
     cout << "N = " << n << endl;
     cout << "lambda = " << lambda << endl;
+
+    // start timing
+    auto start = chrono::high_resolution_clock::now();
+
+    double sum = gauss_quadrature(n, lambda);
+
+    // end timing
+    auto finish = chrono::high_resolution_clock::now();
+
     cout << sum << endl;
 
     double pi = acos(-1);
     double exact = (5*pi*pi)/(16*16);
 
     cout << abs(exact - sum) << endl;
-    */
 
-
-    /*
-    // Improved Gauss Quadrature
-
-    bool timing = true;
-    for (int n=10; n<35 ; n+=5){
-        double sum = improved_gauss_quadrature(n, timing);
-
-        cout << "N = " << n << endl;
-        cout << "Sum: " << sum << endl;
-
-        double pi = acos(-1);
-        double exact = (5*pi*pi)/(16*16);
-
-        cout << "Relative error: " << abs(exact - sum)/exact << "\n" << endl;
+    if (timing) {
+        // print time
+        chrono::duration<double> elapsed = (finish - start);
+        cout << "Time: " << elapsed.count() << " s\n" << endl;
     }
-    */
+}
 
+void print_improved_gauss_quadrature(int n, bool timing) {
+    cout << "Improved Gauss Quadrature" << endl;
 
+    // start timing
+    auto start = chrono::high_resolution_clock::now();
 
-    // Monte Carlo
-    int n = 1000000;
-    double lambda = 1.9;
-    bool timing = true;
+    double sum = improved_gauss_quadrature(n);
 
-    double sum = monte_carlo(n, lambda, timing);
+    // end timing
+    auto finish = chrono::high_resolution_clock::now();
 
     cout << "N = " << n << endl;
-    cout << "lambda = " << lambda << endl;
-    cout << "sum: " << sum << endl;
+    cout << "Sum: " << sum << endl;
 
     double pi = acos(-1);
     double exact = (5*pi*pi)/(16*16);
 
-    cout << "relative error: " << abs(exact - sum)/exact << "\n" << endl;
+    cout << "Relative error: " << abs(exact - sum)/exact << endl;
 
+    if (timing) {
+        // print time
+        chrono::duration<double> elapsed = (finish - start);
+        cout << "Time: " << elapsed.count() << " s\n" << endl;
+    }
+}
 
+void print_monte_carlo(int n, double lambda, bool timing) {
+    double sum, variance, exact;
+    cout << "Brute force Monte Carlo" << endl;
 
+    cout << "N = " << 2*n << endl;
+    cout << "lambda = " << lambda << endl;
 
-    /*
-    // Improved Monte Carlo
+    // start timing
+    auto start = chrono::high_resolution_clock::now();
 
-//        int n = 100000;
-//        bool timing = true;
-        double sum4 = improved_monte_carlo(n, timing);
+    tie(sum, variance) = monte_carlo(2*n, lambda, 1424);
 
+    // end timing
+    auto finish = chrono::high_resolution_clock::now();
+
+    cout << "sum: " << sum << endl;
+
+    double pi = acos(-1);
+    exact = (5*pi*pi)/(16*16);
+
+    cout << "relative error: " << abs(exact - sum)/exact << endl;
+    cout << "variance: " << variance << endl;
+
+    if (timing) {
+        // print time
+        chrono::duration<double> elapsed = (finish - start);
+        cout << "Time: " << elapsed.count() << " s\n" << endl;
+    }
+}
+
+void print_improved_monte_carlo(int n, bool timing) {
+    double sum, variance, exact;
+    cout << "Improved Monte Carlo" << endl;
+
+    // start timing
+    auto start = chrono::high_resolution_clock::now();
+
+    tie(sum, variance) = improved_monte_carlo(n, time_t(0));
+
+    // end timing
+    auto finish = chrono::high_resolution_clock::now();
+
+    cout << "N = " << n << endl;
+    cout << "sum: " << sum << endl;
+
+    double pi = acos(-1);
+    exact = (5*pi*pi)/(16*16);
+
+    cout << "relative error: " << abs(exact - sum)/exact << endl;
+
+    if (timing) {
+        // print time
+        chrono::duration<double> elapsed = (finish - start);
+        cout << "Time: " << elapsed.count() << " s\n" << endl;
+    }
+}
+
+void print_parallellized_monte_carlo(int &nargs, char* args[], int n, double lambda, bool timing) {
+    int numprocs, my_rank, flag;
+    double time_start, time_end, total_time;
+    double total_sum, local_sum, variance;
+
+    //   MPI initializations
+    MPI_Init (&nargs, &args);
+    MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
+
+    // start timing
+    time_start = MPI_Wtime();
+
+    total_sum = 0.0;
+    tie(local_sum, variance) = monte_carlo(n, lambda, time_t(0) + my_rank);
+
+    MPI_Reduce(&local_sum, &total_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    total_sum = total_sum/numprocs;
+
+    // end timing
+    time_end = MPI_Wtime();
+    total_time = time_end-time_start;
+
+    if (my_rank == 0) {
+        cout << "Parallellized Monte Carlo" << endl;
         cout << "N = " << n << endl;
-        cout << "sum: " << sum << endl;
+        cout << "Lambda = " << lambda << endl;
+        cout << "Sum: " << total_sum << endl;
 
-//        double pi = acos(-1);
-//        double exact = (5*pi*pi)/(16*16);
+        double pi = acos(-1);
+        double exact = (5*pi*pi)/(16*16);
 
-        cout << "relative error: " << abs(exact - sum4)/exact << endl;
-     */
+        cout << "Relative Error: " << abs(exact - total_sum)/exact << endl;
+        cout << "Variance: " << variance << endl;
+
+        if (timing) {
+            cout << "Timing: " << total_time << " s\n" << endl;
+        }
+    }
+
+    //  End MPI
+    MPI_Finalize ();
+}
+
+void print_parallellized_improved_monte_carlo(int &nargs, char* args[], int n, bool timing) {
+    int numprocs, my_rank, flag;
+    double time_start, time_end, total_time;
+    double total_sum, local_sum, variance;
+
+    //   MPI initializations
+    MPI_Init (&nargs, &args);
+    MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
+
+    // start timing
+    time_start = MPI_Wtime();
+
+    // Parallellized Monte Carlo
+    total_sum = 0.0;
+    tie(local_sum, variance) = improved_monte_carlo(n, time_t(0) + my_rank);
+
+    MPI_Reduce(&local_sum, &total_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    total_sum = total_sum/numprocs;
+
+    // end timing
+    time_end = MPI_Wtime();
+    total_time = time_end-time_start;
+
+    if (my_rank == 0) {
+        cout << "Parallellized Monte Carlo" << endl;
+        cout << "N = " << 2*n << endl;
+        cout << "Sum: " << total_sum << endl;
+
+        double pi = acos(-1);
+        double exact = (5*pi*pi)/(16*16);
+
+        cout << "Relative Error: " << abs(exact - total_sum)/exact << endl;
+        cout << "Variance: " << variance << endl;
+
+        if (timing) {
+            cout << "Timing: " << total_time << " s\n" << endl;
+        }
+    }
+
+    //  End MPI
+    MPI_Finalize ();
+}
+
+
+int main(int nargs, char* args[]) {
+    int n;
+    double lambda;
+    bool timing;
+
+    n = 100000;
+    lambda = 2.7;
+    timing = true;
+
+//    print_parallellized_monte_carlo(nargs, args, n, lambda, timing);
+
+    print_improved_monte_carlo(n, timing);
 
     return 0;
 }
