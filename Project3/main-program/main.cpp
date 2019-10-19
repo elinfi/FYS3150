58ddbc56d6d6f89b0eabcda67083966d0ca4213a
elinfi <elin.finstad@hotmail.com>
2019-10-19 17:14:14 +0200

    Printing to file

--- Project3/main-program/main.cpp
+++ Project3/main-program/main.cpp
@@ -8,6 +8,8 @@
 #include <random>
 #include <mpi.h>
 #include <chrono>
+#include <tuple>
+#include <fstream>
 
 using namespace std;
 
@@ -65,67 +67,73 @@ void print_improved_gauss_quadrature(int n, bool timing) {
     }
 }
 
-void print_monte_carlo(int n, double lambda, bool timing) {
-    double sum, variance, exact;
+tuple<double, double> print_monte_carlo(int n, double lambda, bool timing, int seed) {
+    double sum, variance, exact, rel_err;
     cout << "Brute force Monte Carlo" << endl;
 
-    cout << "N = " << 2*n << endl;
+    cout << "N = " << n << endl;
     cout << "lambda = " << lambda << endl;
 
     // start timing
     auto start = chrono::high_resolution_clock::now();
 
-    tie(sum, variance) = monte_carlo(2*n, lambda, 1424);
+    tie(sum, variance) = monte_carlo(n, lambda, seed);
 
     // end timing
     auto finish = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = (finish - start);
 
     cout << "sum: " << sum << endl;
 
     double pi = acos(-1);
     exact = (5*pi*pi)/(16*16);
+    rel_err = abs(exact - sum)/exact;
 
-    cout << "relative error: " << abs(exact - sum)/exact << endl;
+    cout << "relative error: " << rel_err << endl;
     cout << "variance: " << variance << endl;
 
     if (timing) {
         // print time
-        chrono::duration<double> elapsed = (finish - start);
         cout << "Time: " << elapsed.count() << " s\n" << endl;
     }
+
+    return make_tuple(rel_err, elapsed.count());
 }
 
-void print_improved_monte_carlo(int n, bool timing) {
-    double sum, variance, exact;
+tuple<double, double> print_improved_monte_carlo(int n, bool timing, int seed) {
+    double sum, variance, exact, rel_err;
     cout << "Improved Monte Carlo" << endl;
 
     // start timing
     auto start = chrono::high_resolution_clock::now();
 
-    tie(sum, variance) = improved_monte_carlo(n, time_t(0));
+    tie(sum, variance) = improved_monte_carlo(n, seed);
 
     // end timing
     auto finish = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = (finish - start);
 
     cout << "N = " << n << endl;
     cout << "sum: " << sum << endl;
 
     double pi = acos(-1);
     exact = (5*pi*pi)/(16*16);
+    rel_err = abs(exact - sum)/exact;
 
-    cout << "relative error: " << abs(exact - sum)/exact << endl;
+    cout << "relative error: " << rel_err << endl;
 
     if (timing) {
         // print time
-        chrono::duration<double> elapsed = (finish - start);
         cout << "Time: " << elapsed.count() << " s\n" << endl;
     }
+
+    return make_tuple(rel_err, elapsed.count());
 }
 
-void print_parallellized_monte_carlo(int &nargs, char* args[], int n, double lambda, bool timing) {
+tuple<double, double> print_parallellized_monte_carlo(int &nargs, char* args[], int n, double lambda, bool timing) {
     int numprocs, my_rank, flag;
     double time_start, time_end, total_time;
-    double total_sum, local_sum, variance;
+    double total_sum, local_sum, variance, rel_err, exact;
 
     //   MPI initializations
     MPI_Init (&nargs, &args);
@@ -153,9 +161,10 @@ void print_parallellized_monte_carlo(int &nargs, char* args[], int n, double lam
         cout << "Sum: " << total_sum << endl;
 
         double pi = acos(-1);
-        double exact = (5*pi*pi)/(16*16);
+        exact = (5*pi*pi)/(16*16);
+        rel_err = abs(exact - total_sum)/exact;
 
-        cout << "Relative Error: " << abs(exact - total_sum)/exact << endl;
+        cout << "Relative Error: " << rel_err << endl;
         cout << "Variance: " << variance << endl;
 
         if (timing) {
@@ -165,12 +174,14 @@ void print_parallellized_monte_carlo(int &nargs, char* args[], int n, double lam
 
     //  End MPI
     MPI_Finalize ();
+
+    return make_tuple(rel_err, total_time);
 }
 
-void print_parallellized_improved_monte_carlo(int &nargs, char* args[], int n, bool timing) {
+tuple<double, double> print_parallellized_improved_monte_carlo(int &nargs, char* args[], int n, bool timing) {
     int numprocs, my_rank, flag;
     double time_start, time_end, total_time;
-    double total_sum, local_sum, variance;
+    double total_sum, local_sum, variance, rel_err, exact;
 
     //   MPI initializations
     MPI_Init (&nargs, &args);
@@ -198,9 +209,10 @@ void print_parallellized_improved_monte_carlo(int &nargs, char* args[], int n, b
         cout << "Sum: " << total_sum << endl;
 
         double pi = acos(-1);
-        double exact = (5*pi*pi)/(16*16);
+        exact = (5*pi*pi)/(16*16);
+        rel_err = abs(exact - total_sum)/exact;
 
-        cout << "Relative Error: " << abs(exact - total_sum)/exact << endl;
+        cout << "Relative Error: " << rel_err << endl;
         cout << "Variance: " << variance << endl;
 
         if (timing) {
@@ -210,6 +222,8 @@ void print_parallellized_improved_monte_carlo(int &nargs, char* args[], int n, b
 
     //  End MPI
     MPI_Finalize ();
+
+    return make_tuple(rel_err, total_time);
 }
 
 
@@ -219,12 +233,24 @@ int main(int nargs, char* args[]) {
     bool timing;
 
     n = 100000;
-    lambda = 2.7;
+    lambda = 1.9;
     timing = true;
+    int seed = 1337;
 
-//    print_parallellized_monte_carlo(nargs, args, n, lambda, timing);
+    double rel_err, time, rel_err_improved, time_improved;
+//    tie(rel_err, time) = print_parallellized_improved_monte_carlo(nargs, args, n, timing);
 
-    print_improved_monte_carlo(n, timing);
+    string filename = "monte_carlo";
+
+    ofstream ofile;
+    ofile.open(filename);
+
+    for (n = 100; n < 1000000000; n*=10) {
+        tie(rel_err, time) = print_monte_carlo(n, lambda, timing, seed);
+        ofile << rel_err << " " << time << "\n";
+    }
+
+    ofile.close();
 
     return 0;
 }
