1c7e11de7e9029e98a34439b741b334d3eacd6ab
elinfi <elin.finstad@hotmail.com>
2019-11-14 17:33:02 +0100

    Adding parallellization with openMP

--- Project4/main-program/main-program.pro
+++ Project4/main-program/main-program.pro
@@ -10,16 +10,21 @@ LIBS += \
         -larmadillo -lblas -llapack
 
 # MPI Settings
-QMAKE_CXX = mpicxx
-QMAKE_CXX_RELEASE = $$QMAKE_CXX
-QMAKE_CXX_DEBUG = $$QMAKE_CXX
-QMAKE_LINK = $$QMAKE_CXX
-QMAKE_CC = mpicc
+#QMAKE_CXX = mpicxx
+#QMAKE_CXX_RELEASE = $$QMAKE_CXX
+#QMAKE_CXX_DEBUG = $$QMAKE_CXX
+#QMAKE_LINK = $$QMAKE_CXX
+#QMAKE_CC = mpicc
 
-QMAKE_CFLAGS += $$system(mpicc --showme:compile)
-QMAKE_LFLAGS += $$system(mpicxx --showme:link)
-QMAKE_CXXFLAGS += $$system(mpicxx --showme:compile) -DMPICH_IGNORE_CXX_SEEK
-QMAKE_CXXFLAGS_RELEASE += $$system(mpicxx --showme:compile) -DMPICH_IGNORE_CXX_SEEK
+#QMAKE_CFLAGS += $$system(mpicc --showme:compile)
+#QMAKE_LFLAGS += $$system(mpicxx --showme:link)
+#QMAKE_CXXFLAGS += $$system(mpicxx --showme:compile) -DMPICH_IGNORE_CXX_SEEK
+#QMAKE_CXXFLAGS_RELEASE += $$system(mpicxx --showme:compile) -DMPICH_IGNORE_CXX_SEEK
+
+# OpenMP settings
+QMAKE_CXXFLAGS += -fopenmp
+QMAKE_LFLAGS += -fopenmp
+LIBS += -fopenmp
 
 SOURCES += \
         mc_metropolis.cpp \
@@ -29,3 +34,7 @@ SOURCES += \
 HEADERS += \
     mc_metropolis.h \
     visualize.h
+
+DISTFILES += \
+    plot_4e.py \
+    plotting.py
--- Project4/main-program/main.cpp
+++ Project4/main-program/main.cpp
@@ -1,40 +1,103 @@
 #include <iostream>
 #include "mc_metropolis.h"
-#include <mpi.h>
+//#include <mpi.h>
 #include <fstream>
-
+#include <cmath>
+#include <armadillo>
 using namespace std;
 
-int main(int args, char* argv[])
+int main()
 {
-    MPI_Init(&args, &argv);
-    int L = 2;
-    int N = 1000000;
-    double temp = 1;
+//    int numprocs, my_rank;
+
+//    MPI_Init(&args, &argv);
+//    MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
+//    MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
+
+    int N = 50000;
+    int L = 40;
     bool random_spin = true;
-
-    tuple <double, double, double, double, double> values = markov_chain(N, L, temp, random_spin);
-
     double E, E_variance, M, M_variance, M_abs;
 
-//    E = get<0>(values);
-//    E_variance = get<1>(values);
-//    M = get<2>(values);
-//    M_variance = get<3>(values);
-//    M_abs = get<4>(values);
+    double t_start = 2.2;
+    double t_end = 2.4;
+    double step = 0.01;
+    int n = ceil((t_end - t_start)/step) + 1;
 
-    tie(E, E_variance, M, M_variance, M_abs) = values;
+    double t_list[n];
+    for(int i=0; i<n; i++){
+        t_list[i] = t_start + step*i;
+        cout << t_list[i] << endl;
+    }
 
-    MPI_Finalize();
-
-    double Cv = E_variance/(temp*temp);         // heat capacity
-    double Chi = M_variance/(temp*temp);        // susceptibility
-
-    cout << Cv << " " << Chi << endl;
-
-    string file = "4b";
+    string file = "4e_L40_N5e5_2_23_01_.txt";
     ofstream ofile;
     ofile.open(file);
+    ofile << n << endl;
+
+    #pragma omp parallel for
+    for (int i=0; i<n; i++) {
+        double t = t_list[i];
+        tuple <double, double, double, double, double> values = markov_chain(N, L, t, random_spin);
+        tie(E, E_variance, M, M_variance, M_abs) = values;
+
+        double Cv = E_variance/(t*t);         // heat capacity
+        double Chi = M_variance/(t*t);        // susceptibility
+        cout << t << " " << E << " " << M_abs << " " << Cv << " " << Chi << endl;
+        ofile << t << " " << E << " " << M_abs << " " << Cv << " " << Chi << endl;
+    }
+
+
+
+
+//    double temp = 2.4;
+//    double beta = 1/temp;
+//    bool random_spin = true;
+
+//    double E, E_variance, M, M_variance, M_abs;
+//    double exact_E, exact_M_abs;
+
+//    exact_E = -(8*cosh(8*beta)/(cosh(8*beta) + 3));
+//    exact_M_abs = (2*exp(8*beta) + 4)/(cosh(8*beta) + 3);
+
+//    cout << exact_E/4 << " " << exact_M_abs/4 << endl;
+
+    /*
+    for (int N=100; N<100000; N*=10) {
+        tuple <double, double, double, double, double> values = markov_chain(N, L, temp, random_spin);
+        tie(E, E_variance, M, M_variance, M_abs) = values;
+        double E_err = abs(exact_E - E)/exact_E;
+        double M_err = abs(exact_M_abs - M_abs)/exact_M_abs;
+        cout << "N: " << N << " Energy: " << E << " E_err: " << E_err <<  " Magnet: " << M_abs << " M_err: " << M_err <<endl;
+    }
+    */
+
+
+
+
+//    cout << Cv << " " << Chi << endl;
+
+
+
+
+
+    /*
+    string file = "4c_T24_10_5e5_300_true.txt";
+    ofstream ofile;
+    ofile.open(file);
+
+    int start = 10;
+    int end = 50000;
+    int step = 300;
+    ofile << (end-start)/step << endl;
+    for (int N=start; N<end; N+=step) {
+        tuple <double, double, double, double, double> values = markov_chain(N, L, temp, random_spin);
+        tie(E, E_variance, M, M_variance, M_abs) = values;
+        ofile << N << " " << E << " " << M_abs << endl;
+    }
+    */
+
+//    MPI_Finalize();
 
 
     return 0;
