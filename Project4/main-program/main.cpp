9614afc6492b71b0a15b83b1e68f64b5ebef5e00
elinfi <elin.finstad@hotmail.com>
2019-11-15 10:18:28 +0100

    Changed for loop

--- Project4/main-program/main.cpp
+++ Project4/main-program/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "mc_metropolis.h"
-//#include <mpi.h>
 #include <fstream>
 #include <cmath>
 #include <armadillo>
@@ -8,38 +7,29 @@ using namespace std;
 
 int main()
 {
-//    int numprocs, my_rank;
-
-//    MPI_Init(&args, &argv);
-//    MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
-//    MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
 
     int N = 50000;
-//    int L = 40;
-    bool random_spin = true;
+    bool random_spin = false;
     double E, E_variance, M, M_variance, M_abs;
 
-    double t_start = 2.2;
+    double t_start = 2.1;
     double t_end = 2.4;
-    double step = 0.01;
+    double step = 0.001;
     int n = ceil((t_end - t_start)/step) + 1;
 
     double t_list[n];
     for(int i=0; i<n; i++){
         t_list[i] = t_start + step*i;
-        cout << t_list[i] << endl;
     }
 
     string file;
     ofstream ofile;
-    ofile.open(file);
-    ofile << n << endl;
-
-    #pragma omp parallel for
-    for (int L = 40; L < 110; L+=20) {
-        file = "4e_" + to_string(L) + "_N5e5_22_24_01.txt";
+    int L = 40;
+//    for (int L = 40; L < 50; L+=20) {
+        file = "4e_L" + to_string(L) + "_N5e5_21_24_001_.txt";
         ofile.open(file);
         ofile << n << endl;
+        #pragma omp parallel for
         for (int i=0; i<n; i++) {
             double t = t_list[i];
             tuple <double, double, double, double, double> values = markov_chain(N, L, t, random_spin);
@@ -50,7 +40,8 @@ int main()
             cout << t << " " << E << " " << M_abs << " " << Cv << " " << Chi << endl;
             ofile << t << " " << E << " " << M_abs << " " << Cv << " " << Chi << endl;
         }
-    }
+//    }
+
 
 
 
@@ -83,26 +74,29 @@ int main()
 //    cout << Cv << " " << Chi << endl;
 
 
-
-
-
     /*
-    string file = "4c_T24_10_5e5_300_true.txt";
+    string file = "4c_T24_10_5e5_300_true_L40.txt";
     ofstream ofile;
     ofile.open(file);
 
+    int L = 40;
+    double temp = 2.4;
+    bool random_spin = true;
+    double E, E_variance, M, M_variance, M_abs;
+
     int start = 10;
     int end = 50000;
-    int step = 300;
+    int step = 500;
+
+
     ofile << (end-start)/step << endl;
+    #pragma omp parallel for
     for (int N=start; N<end; N+=step) {
         tuple <double, double, double, double, double> values = markov_chain(N, L, temp, random_spin);
         tie(E, E_variance, M, M_variance, M_abs) = values;
         ofile << N << " " << E << " " << M_abs << endl;
     }
     */
-
-//    MPI_Finalize();
 
 
     return 0;
