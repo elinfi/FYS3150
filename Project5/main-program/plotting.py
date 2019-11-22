7fa720556678c909d029bdd75d58df453f744923
elinfi <elin.finstad@hotmail.com>
2019-11-22 14:44:33 +0100

    Adding file for plotting histogram

--- Project5/main-program/plotting.py
+++ Project5/main-program/plotting.py
@@ -1,4 +1,19 @@
-# This Python file uses the following encoding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
 
-# if__name__ == "__main__":
-#     pass
+data = np.loadtxt("5a")
+print(data.shape)
+
+max = np.max(data)
+min = np.min(data)
+
+bins = int((max - min)*0.05)
+print(bins)
+plt.hist(data.flatten(), bins, (min, max), density=True)
+
+m = np.linspace(0, max, 10000)
+beta = 1/500
+plt.plot(m, beta*np.exp(-beta*m))
+plt.show()
+
+
