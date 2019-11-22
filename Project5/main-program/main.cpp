fc348e9ae85ddafb635499419107890d41afa18f
elinfi <elin.finstad@hotmail.com>
2019-11-22 14:45:08 +0100

    Call transactions write output to file

--- Project5/main-program/main.cpp
+++ Project5/main-program/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <transactions.h>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    int N = 500;
+    double m0 = 500;
+    int txn = 1e7;
+    int runs = 10;
+
+    string filename = "5a";
+    ofstream ofile;
+    ofile.open(filename);
+
+    for (int i=0; i<runs; i++) {
+        transactions(N, m0, txn, ofile);
+    }
 }
