e731f1431e7ae1a3f7175f83979dea1348d8a39b
elinfi <elin.finstad@hotmail.com>
2019-11-28 13:11:06 +0100

    Updated for saving and run over all agents

--- Project5/main-program/transactions.cpp
+++ Project5/main-program/transactions.cpp
@@ -8,7 +8,7 @@ using namespace std;
 using namespace arma;
 
 
-void transactions(int N, double m0, double txn, ofstream& ofile) {
+void transactions(int N, double m0, double lambda, double txn, ofstream& ofile) {
     int seed = time(0);
 
     // generate engine
@@ -16,7 +16,6 @@ void transactions(int N, double m0, double txn, ofstream& ofile) {
 
     // generate integers in interval [0, N-1] with uniform distribution
     uniform_int_distribution<int> int_uniform(0, N-1);
-
     uniform_int_distribution<int> int2_uniform(0, N-2);
 
     // generate double with uniform distribution
@@ -24,30 +23,47 @@ void transactions(int N, double m0, double txn, ofstream& ofile) {
 
     // Store the 500 agents in a vector with initial amount of money m0
     vec m = zeros<vec>(N);
-    m.fill(m0);
+    vec m_mean = zeros<vec>(N);
+    m_mean.fill(m0);
 
-    for (int t=0; t<txn; t++) {
-        // choose a random pair (i, j)
-        int i = int_uniform(engine);
-        int j = int2_uniform(engine);
-        if (j >= i) {
-            j += 1;
+    for (int n=0; n<N; n++) {
+        m.fill(m0);
+        for (int t=0; t<txn; t++) {
+            // choose a random pair (i, j)
+            int i = int_uniform(engine);
+            int j = int2_uniform(engine);
+            if (j >= i) {
+                j += 1;
+            }
+
+            // choose a random reassignment eps
+            double eps = double_uniform(engine);
+
+            // after transaction
+            double dm = (1 - lambda)*(eps*m[j] - (1 - eps)*m[i]);
+            m[i] += dm;
+            m[j] -= dm;
+
+            m_mean[i] += m[i];
+            m_mean[j] += m[j];
         }
-
-        // choose a random reassignment eps
-        double eps = double_uniform(engine);
-
-        // after transaction
-        double mi_new = eps*(m[i] + m[j]);
-        double mj_new = (1 - eps)*(m[i] + m[j]);
-
-        m[i] = mi_new;
-        m[j] = mj_new;
     }
 
+    m_mean /= N;
+
+    double mean = 0;
+    double mean2 = 0;
     for (int i=0; i<N-1; i++) {
-        ofile << m[i] << " ";
+        mean += m_mean[i];
+        mean2 += m_mean[i]*m_mean[i];
+//        ofile << m_mean[i] << " ";
     }
-    ofile << "\n";
+//    ofile << "\n";
+
+    mean /= N;
+    mean2 /= N;
+
+    double var = mean2 - mean*mean;
+    ofile <<txn << " " << var << endl;
 
 }
