0dde091dbc68a097f72e97e68dc15ac4edd24ea2
elinfi <elin.finstad@hotmail.com>
2019-12-12 11:31:12 +0100

    Changes for new printing of data files

--- Project5/main-program/main-program.pro
+++ Project5/main-program/main-program.pro
@@ -3,5 +3,20 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
+INCLUDEPATH += C:\armadillo-9.600.6\include
+DEPENDPATH += C:\armadillo-9.600.6\include
+
+LIBS += \
+        -larmadillo -lblas -llapack
+
 SOURCES += \
-        main.cpp
+        main.cpp \
+        transactions.cpp
+
+HEADERS += \
+    transactions.h
+
+DISTFILES += \
+    plot_5c.py \
+    plot_variance.py \
+    plotting.py
--- Project5/main-program/main.cpp
+++ Project5/main-program/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <transactions.h>
+#include "transactions.h"
 #include <fstream>
 
 using namespace std;
@@ -8,14 +8,32 @@ int main()
 {
     int N = 500;
     double m0 = 500;
-    int txn = 1e7;
-    int runs = 10;
+    double lambda = 0.25;
+    double alpha = 1.5;
+    double gamma = 0.0;
+    int txn = 1e4;
+    int mc = 1e3;
 
-    string filename = "5a";
+//    string filename = "var_N" + to_string(N) + "_m0" + to_string(int(m0)) + "_lambda" + to_string(lambda) + "_txn" + to_string(txn) + "_runs" + to_string(runs) + ".txt";
+    string filename = "5d_N500_m500_l025_a15_g0_25e4_1e7.txt";
+    cout << filename << endl;
     ofstream ofile;
     ofile.open(filename);
 
-    for (int i=0; i<runs; i++) {
-        transactions(N, m0, txn, ofile);
+    for (int i=25000; i<1e7; i+=50000) {
+        transactions(N, m0, lambda, alpha, gamma, i, ofile);
     }
+//    for (int i=0; i<mc; i++) {
+//        transactions(N, m0, lambda, alpha, gamma, txn, ofile);
+//    }
 }
+
+/*
+ * Beregn variansen for lambda = 0.25, 0.5, 0.9. - finn beste antall transaksjoner
+ * Få svar på hva paramteriseringen i 5c betyr. High-end tails.
+ * 5d, hva er "perform the same analysis as previously"?
+ * 5d extract the tail og sammenlikn med Pareto dsitribution
+ * 5d - kjøre for N=500, 1000 og lambda = 0.25, 0.5, 0.9 og alpha = 0.5, 1.0, 1.5,  2.0???
+ * 5e wealth distribution?
+*/
+
