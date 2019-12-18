cf41371efd94d80b9cd1e773334a28b283cf8ccf
elinfi <elin.finstad@hotmail.com>
2019-12-18 15:56:36 +0100

    Adding code for plotting

--- Project5/main-program/main.cpp
+++ Project5/main-program/main.cpp
@@ -6,34 +6,75 @@ using namespace std;
 
 int main()
 {
-    int N = 500;
+    int N = 1000;
     double m0 = 500;
-    double lambda = 0.25;
-    double alpha = 1.5;
+    double lambda = 0.9;
+    double alpha = 2.0;
     double gamma = 0.0;
-    int txn = 1e4;
-    int mc = 1e3;
+    int txn = 1e7;
+    int mc = 1e4;
 
-//    string filename = "var_N" + to_string(N) + "_m0" + to_string(int(m0)) + "_lambda" + to_string(lambda) + "_txn" + to_string(txn) + "_runs" + to_string(runs) + ".txt";
-    string filename = "5d_N500_m500_l025_a15_g0_25e4_1e7.txt";
+    string filename = "5e_N1000_m500_l09_a2_g0_2.txt";
     cout << filename << endl;
     ofstream ofile;
     ofile.open(filename);
 
-    for (int i=25000; i<1e7; i+=50000) {
-        transactions(N, m0, lambda, alpha, gamma, i, ofile);
-    }
-//    for (int i=0; i<mc; i++) {
-//        transactions(N, m0, lambda, alpha, gamma, txn, ofile);
-//    }
-}
+    /*
+    int t = 5;
+    while (t < 1e8) {
+            transactions(N, m0, lambda, alpha, gamma, t, true, false, ofile);
 
-/*
- * Beregn variansen for lambda = 0.25, 0.5, 0.9. - finn beste antall transaksjoner
- * Få svar på hva paramteriseringen i 5c betyr. High-end tails.
- * 5d, hva er "perform the same analysis as previously"?
- * 5d extract the tail og sammenlikn med Pareto dsitribution
- * 5d - kjøre for N=500, 1000 og lambda = 0.25, 0.5, 0.9 og alpha = 0.5, 1.0, 1.5,  2.0???
- * 5e wealth distribution?
-*/
+            if (t < 10000) {
+                t += 1000;
+            }
+            else if (t > 10000 && t < 5e5) {
+                t += 50000;
+            }
+            else if (t > 5e5 && t < 1e6) {
+                t += 70000;
+            }
+            else if (t > 1e6 && t < 4e6) {
+                t += 200000;
+            }
+            else if (t > 4e6 && t < 1e7) {
+                t += 400000;
+            }
+            else if (t > 1e7 && 5e7) {
+                t += 500000;
+            }
+            else {
+                t += 600000;
+            }
+    }
+    */
+
+    /*
+    int t = 5;
+    while (t < 1e7) {
+            transactions(N, m0, lambda, alpha, gamma, t, true, false, ofile);
+
+            if (t < 10000) {
+                t += 500;
+            }
+            else if (t > 10000 && t < 5e5) {
+                t += 2000;
+            }
+            else if (t > 5e5 && t < 1e6) {
+                t += 7000;
+            }
+            else if (t > 1e6 && t < 4e6) {
+                t += 25000;
+            }
+            else {
+                t += 50000;
+            }
+    }
+    */
+
+    /*
+    for (int i=0; i<mc; i++) {
+        transactions(N, m0, lambda, alpha, gamma, txn, false, true, ofile);
+    }
+    */
+}
 
